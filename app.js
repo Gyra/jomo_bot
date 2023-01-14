3fe4e5d5f4b713b6654ad45c3afc77fb15d1aa7c
Handing <sun.handing@outlook.com>
2023-01-14 22:56:08 +0000

    add slash

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const dotenv = require('dotenv');
-dotenv.config();
-const {REST, Routes, Client, GatewayIntentBits, Events, Guild} = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const {Collection, Client, GatewayIntentBits, Events} = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const internal = require('node:stream');
 
+dotenv.config();
 TOKEN = process.env.DISCORD_TOKEN
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, c => {
     console.log(`Ready ! Logged in as ${c.user.tag}`);
@@ -11,7 +15,32 @@ client.once(Events.ClientReady, c => {
 
 client.login(TOKEN);
 
-client.on('message', message => {
-    const listMbrs = message.guild.roles.get("1063843040768303154").members.map(m => m.user.tag).join('\n');
-    message.channel.send(listMbrs);
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error(`No command matching ${interactioin.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true});
+    }
 });
